703ccaadb2395fadc036677fa5ae405f37462da0
stijnavdl <stijn@orikami.nl>
2022-02-07 11:49:17 +0100

    try react-router

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,43 @@
-import ReactDOM from 'react-dom'
-import App from '/src/viewmodel/App'
+import { render } from 'react-dom'
+import App from '/src/components/App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-function startWeb() {
-  ReactDOM.render(<App />, document.getElementById('app'))
-}
+import Expenses from '/src/routes/expenses'
+import Invoices from '/src/routes/invoices'
 
-if (window.cordova) {
-  document.addEventListener(
-    'deviceready',
-    () => {
-      codePush.sync()
-      startWeb()
-    },
-    false,
-  )
+const rootElement = document.getElementById('app')
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement,
+)
 
-  document.addEventListener(
-    'resume',
-    () => {
-      codePush.sync()
-    },
-    false,
-  )
-} else {
-  startWeb()
-}
+// function startWeb() {
+//   ReactDOM.render(<App />, document.getElementById('app'))
+// }
+
+// if (window.cordova) {
+//   document.addEventListener(
+//     'deviceready',
+//     () => {
+//       codePush.sync()
+//       startWeb()
+//     },
+//     false,
+//   )
+
+//   document.addEventListener(
+//     'resume',
+//     () => {
+//       codePush.sync()
+//     },
+//     false,
+//   )
+// } else {
+//   startWeb()
+// }
